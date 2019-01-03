1c9c9c6706b031dd6f686bb04dd9a928ba81859e
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-01-03 21:26:37 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,15 @@
 # Create your costume_stores migration here
+class CreateCostumeStores < ActiveRecord::Migration[4.2]
+  def change
+    create_table :costume_stores do |s|
+      s.string :name
+      s.string :location
+      s.integer :number_of_costumes
+      s.integer :number_of_employees
+      s.boolean :still_in_business
+      s.string :opening_time
+      s.string :closing_time
+    end
+  end
+
+end
