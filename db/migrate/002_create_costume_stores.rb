3229e424f3d76a0e0fcdb7c866ff6a4ee61c6c68
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-01-03 21:30:12 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -4,11 +4,11 @@ class CreateCostumeStores < ActiveRecord::Migration[4.2]
     create_table :costume_stores do |s|
       s.string :name
       s.string :location
-      s.integer :number_of_costumes
-      s.integer :number_of_employees
+      s.integer :costume_inventory
+      s.integer :num_of_employees
       s.boolean :still_in_business
-      s.string :opening_time
-      s.string :closing_time
+      s.datetime :opening_time
+      s.datetime :closing_time
     end
   end
 
